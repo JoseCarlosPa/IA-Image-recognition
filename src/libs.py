6809f4955cadb7a1e9e7fd1c37cc6694fff3b6df
JoseCarlosPa <josecarlospas1@gmail.com>
2021-04-17 16:07:45 -0500

    Separar scripts para ordenacion y uso de funciones

---
+++ src/libs.py
@@ -0,0 +1,58 @@
+"""
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++                                                                                                                      +
++   ITESM QRO. 2021 - Inteligent Systems                                                                               +
++   Name: Jose Carlos Pacheco Sanchez                                                                                  +
++   ID: A01702828                                                                                                      +
++                                                                                                                      +
++   Script Ob: Cleaner and direct use of funcitions for ploting and framework usage                                    +
++                                                                                                                      +
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+"""
+
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class_names = [
+    'Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six',
+    'Seven', 'Eight', 'Nine'
+]
+
+# split the pixel numbers into black or wihte (255 to 0 or 1)
+def imageWitheBlak(images, labels):
+    images = tf.cast(images, tf.float32)
+    images /= 255
+    return images, labels
+
+
+def plotImages(i, predictions, finalLabel):
+    predictions, finalLabel = predictions[i], finalLabel[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions, color="#888888")
+    plt.ylim([0, 1])
+    finalLabels = np.argmax(predictions)
+
+    thisplot[finalLabels].set_color('red')
+    thisplot[finalLabel].set_color('blue')
+
+
+def plotGraph(i, predictions, finalLabels, images):
+    predictions, finalLabel, img = predictions[i], finalLabels[i], images[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img[..., 0], cmap=plt.cm.binary)
+
+    finalLabels = np.argmax(predictions)
+    if finalLabels == finalLabel:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("Prediccion: {}".format(class_names[finalLabels]), color=color)
--- src/main.py
+++ src/main.py
@@ -1,28 +1,87 @@
+"""
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++                                                                                                                      +
++   ITESM QRO. 2021 - Inteligent Systems                                                                               +
++   Name: Jose Carlos Pacheco Sanchez                                                                                  +
++   ID: A01702828                                                                                                      +
++                                                                                                                      +
++   Script Ob: Main usage of the Script and variant desition on epoc and block nunbers learning                        +
++                                                                                                                      +
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+"""
+
 from __future__ import absolute_import, division, print_function, unicode_literals
+# TensorFlow and KERAS Framework
+import tensorflow_datasets as tfds
+from libs import *
 
-# TensorFlow Framework
-import tensorflow as tf
-import tensorflow_datasets as tfds #Tensorflow dataset usage
-
-# Basic Libraries for calculus and ploting
+# Basic Libraries
+import logging
+import matplotlib.pyplot as plt
 import math
 import numpy as np
-import matplotlib.pyplot as plt
 
-import logging
 logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
 
-# Data set inizialtiation with MNIST
 dataset, metadata = tfds.load('mnist', as_supervised=True, with_info=True)
-train_dataset, test_dataset = dataset['train'], dataset['test']
+datasetTesting, datasetTest = dataset['train'], dataset['test']
 
-# Number class identification
-number_class = [
-    'Cero', 'Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco', 'Seis',
-    'Siete', 'Ocho', 'Nueve'
-]
+# Initializaction of test numbers
+number_train = metadata.splits['train'].num_examples
+number_examples = metadata.splits['test'].num_examples
+datasetTesting = datasetTesting.map(imageWitheBlak)
+datasetTest = datasetTest.map(imageWitheBlak)
 
-# Training dataset and test from MNIST
-num_train_examples = metadata.splits['train'].num_examples
-num_test_examples = metadata.splits['test'].num_examples
+# General initiation of the neural Network with the number of layers
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+    tf.keras.layers.Dense(64, activation=tf.nn.relu),
+    tf.keras.layers.Dense(64, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)  # Clasiffication
+])
+
+# Compile function to use
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# Learning rate into 32 blocks
+size = 32
+datasetTesting = datasetTesting.repeat().shuffle(number_train).batch(size)
+datasetTest = datasetTest.batch(size)
+
+# Start the Model learning process
+model.fit(
+    datasetTesting, epochs=1,  # Number of epocs to use
+    steps_per_epoch=math.ceil(number_train / size)
+)
+
+# Evaluation of the Model
+loss, accuracy = model.evaluate(
+    datasetTest, steps=math.ceil(number_examples / 32)
+)
+
+print("Test Results!!: ", accuracy)
+
+# From the dataset Image we take each one and testit on the labels
+
+for images, labeles in datasetTest.take(1):
+    images = images.numpy()
+    labeles = labeles.numpy()
+    predictions = model.predict(images)
+
+rows = 5
+cols = 3
+total = rows * cols
+
+plt.figure(figsize=(2 * 2 * cols, 2 * rows))
+for i in range(total):
+    plt.subplot(rows, 2 * cols, 2 * i + 1)
+    plotGraph(i, predictions, labeles, images)
+    plt.subplot(rows, 2 * cols, 2 * i + 2)
+    plotImages(i, predictions, labeles)
+
+plt.show()
